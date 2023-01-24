2ddc09f5410db8c3280601c2c9dfd5f2783a0ecc
lizavetion <117774113+lizavetion@users.noreply.github.com>
2023-01-24 15:15:07 +0000

    Deploying to gh-pages from @ lizavetion/group10_icecream@1748d06b02c4180ce6e4ecff4978771dcc527e26 🚀

---
+++ index.a14f898d.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-pc-open]"),closeModalBtn:document.querySelector("[data-modal-pc-close]"),modal:document.querySelector("[data-modal-pc]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.a14f898d.js.map
