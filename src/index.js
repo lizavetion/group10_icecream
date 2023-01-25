411684a1b646634e1508a7f4b44405b74aa83eed
lizavetion <alizafox2000@gmail.com>
2023-01-25 19:27:08 +0200

    Updated modals

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './js/mobile-menu';
 import './js/slider';
 import './js/gallery';
-import "./js/modal";
+import './js/modal';
 import './js-modal-product-card/modal-pc';
-
+import './js-location-franchise-modals/modal-franchise';
+import './js-location-franchise-modals/modal-location';
