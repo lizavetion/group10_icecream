031fca3d72db861695154e354d614b8bb0290dfe
Lyuobomyr Didyk <didyksdb@gmail.com>
2023-01-25 11:47:57 +0100

    зміна місця файлу в index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './js/mobile-menu';
-import './js-modal-product-card/modal-pc';
 import './js/slider';
 import './js/gallery';
 import "./js/modal";
+import './js-modal-product-card/modal-pc';
 
